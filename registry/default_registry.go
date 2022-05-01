df1f95076a758f0852301d7532da17782b1051e8
Trang <40815729+trangmaiq@users.noreply.github.com>
2022-05-01 21:12:48 +0700

    implement create url (#3)

--- registry/default_registry.go
+++ registry/default_registry.go
@@ -2,17 +2,20 @@ package registry
 
 import (
 	"context"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sony/sonyflake"
 	"github.com/trangmaiq/short/internal/handler/url"
-	"github.com/trangmaiq/short/internal/persistence/inmem"
+	"github.com/trangmaiq/short/internal/persistence/cassandra"
 )
 
 var _ Registry = new(DefaultRegistry)
 
 type DefaultRegistry struct {
 	engine    *gin.Engine
-	persister *inmem.Persister
+	persister *cassandra.Persister
+	sonyflake *sonyflake.Sonyflake
 
 	urlRouterGroup *gin.RouterGroup
 }
@@ -30,6 +33,14 @@ func (r *DefaultRegistry) Init(ctx context.Context, engine *gin.Engine) error {
 	r.engine = engine
 	r.urlRouterGroup = engine.Group("/urls")
 
+	persister, err := cassandra.NewPersister()
+	if err != nil {
+		return err
+	}
+	r.persister = persister
+
+	r.sonyflake = sonyflake.NewSonyflake(sonyflake.Settings{StartTime: time.Now()})
+
 	r.URLHandler().RegisterRoutes()
 
 	return nil
@@ -45,4 +56,8 @@ func (r *DefaultRegistry) URLRoutes() gin.IRoutes {
 
 func (r *DefaultRegistry) URLPersister() url.Persister {
 	return r.persister
+}
+
+func (r *DefaultRegistry) KeyGenerator() url.KeyGenerator {
+	return r.sonyflake
 }
